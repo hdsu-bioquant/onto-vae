7c0533d273571cf5df36cf117e545ad89eed0235
daria-dc <daria.doncevic@gmail.com>
2022-05-02 12:09:48 +0200

    add model code

---
+++ onto_vae/model.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python3
+
+# Class for OntoVAE
+
+import numpy as np
+import torch
+from torch import optim
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+
+from .modules import Encoder, Decoder, OntoEncoder, OntoDecoder
+
+
+###-------------------------------------------------------------###
+##           VAE WITH ONTOLOGY IN ENCODER OR DECODER             ##
+###-------------------------------------------------------------###
+
+class OntoVAE(nn.Module):
+    """
+    This class combines a normal encoder with an ontology structured decoder
+
+    Parameters
+    -------------
+    in_features: # of features that are used as input
+    onto: 'encoder' or 'decoder', indicating which takes ontology structure
+    layer_dims_dec: list giving the dimensions of the layers in the decoder
+    mask_list: matrix for each layer transition, that determines which weights to zero out
+    layer_dims_enc: list giving the dimensions of the layers in the encoder
+    neuronnum: number of neurons per term
+    drop: dropout rate, default is 0
+    z_drop: dropout rate for latent space, default is 0.5
+    """
+
+    def __init__(self, onto, in_features, layer_dims_enc, layer_dims_dec, mask_list, neuronnum=1, drop=0, z_drop=0.5):
+        super(OntoVAE, self).__init__()
+
+        self.onto = onto
+        self.in_features = in_features
+        self.layer_dims_enc = list(np.array(layer_dims_enc) * neuronnum)
+        self.layer_dims_dec = layer_dims_dec
+        self.mask_list = mask_list
+        if self.onto == 'encoder':
+            self.latent_dim = layer_dims_enc[-1] * neuronnum
+        else:
+            self.latent_dim = layer_dims_dec[0] * neuronnum
+        self.neuronnum = neuronnum
+        self.drop = drop
+        self.z_drop = z_drop
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    
+        if self.onto == 'encoder':
+            # Encoder
+            self.encoder = OntoEncoder(self.in_features,
+                                        self.layer_dims_enc,
+                                        self.mask_list,
+                                        self.drop,
+                                        self.z_drop)
+
+            # Decoder
+            self.decoder = Decoder(self.in_features,
+                                   self.latent_dim,
+                                   self.layer_dims_dec,
+                                   self.drop)
+        else:
+            # Encoder
+            self.encoder = Encoder(self.in_features,
+                                self.latent_dim,
+                                self.layer_dims_enc,
+                                self.drop,
+                                self.z_drop)
+
+            # Sigmoid layer
+            #self.sigmoid = nn.Sigmoid()
+
+            # Decoder
+            self.decoder = OntoDecoder(self.in_features,
+                                    self.layer_dims_dec,
+                                    self.mask_list,
+                                    self.latent_dim,
+                                    self.neuronnum,
+                                    self.drop)
+        
+    def reparameterize(self, mu, log_var):
+        """
+        Parameters
+        -------------
+        mu: mean from the encoder's latent space
+        log_var: log variance from the encoder's latent space
+        """
+        sigma = torch.exp(0.5*log_var) 
+        eps = torch.randn_like(sigma) 
+        return mu + eps * sigma
+        
+    def get_embedding(self, x):
+        mu, log_var = self.encoder(x)
+        embedding = self.reparameterize(mu, log_var)
+        return embedding
+
+    def forward(self, x):
+        # encoding
+        mu, log_var = self.encoder(x)
+            
+        # sample from latent space
+        z = self.reparameterize(mu, log_var)
+        
+        # apply sigmoid layer on z
+        #z = self.sigmoid(z)
+
+        # decoding
+        reconstruction = self.decoder(z)
+            
+        return reconstruction, mu, log_var
+
+    def vae_loss(self, reconstruction, mu, log_var, data, kl_coeff):
+        #rec_loss = F.mse_loss(reconstruction, data, reduction='sum')/data.shape[0]
+        #kl_loss = torch.mean(-0.5 *  torch.sum(1 + log_var - mu.pow(2) - log_var.exp(), dim=1), dim=0)
+        #loss = rec_loss + kl_coeff * kl_loss
+        #return loss
+        kl_loss = -0.5 * torch.sum(1. + log_var - mu.pow(2) - log_var.exp(), )
+        rec_loss = F.mse_loss(reconstruction, data, reduction="sum")
+        return torch.mean(rec_loss + kl_coeff*kl_loss)
+
+    def train_round(self, dataloader, lr, kl_coeff, optimizer):
+        """
+        Parameters
+        -------------
+        dataloader: pytorch dataloader instance with training data
+        lr: learning rate
+        kl_coeff: coefficient for weighting Kullback-Leibler loss
+        optimizer: optimizer for training
+        """
+        # set to train mode
+        self.train()
+
+        # initialize running loss
+        running_loss = 0.0
+
+        # iterate over dataloader for training
+        for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
+
+            # move batch to device
+            data = data[0].to(self.device)
+            optimizer.zero_grad()
+
+            # forward step
+            reconstruction, mu, log_var = self.forward(data)
+            loss = self.vae_loss(reconstruction, mu, log_var, data, kl_coeff)
+            running_loss += loss.item()
+
+            # backward propagation
+            loss.backward()
+
+            # zero out gradients from non-existent connections
+            if self.onto == 'decoder':
+                for i in range(len(self.decoder.decoder)):
+                    self.decoder.decoder[i][0].weight.grad = torch.mul(self.decoder.decoder[i][0].weight.grad, self.decoder.masks[i])
+            else:
+                for i in range(len(self.encoder.encoder)):
+                    self.encoder.encoder[i][0].weight.grad = torch.mul(self.encoder.encoder[i][0].weight.grad, self.encoder.masks[i])
+
+                # apply mask on latent space
+                self.encoder.mu[0].weight.grad = torch.mul(self.encoder.mu[0].weight.grad, self.encoder.masks[-1])
+                self.encoder.logvar[0].weight.grad = torch.mul(self.encoder.logvar[0].weight.grad, self.encoder.masks[-1])
+
+            # perform optimizer step
+            optimizer.step()
+
+            # make weights in Onto module positive
+            if self.onto == 'decoder':
+                for i in range(len(self.decoder.decoder)):
+                    self.decoder.decoder[i][0].weight.data = self.decoder.decoder[i][0].weight.data.clamp(0)
+            else:
+                for i in range(len(self.encoder.encoder)):
+                    self.encoder.encoder[i][0].weight.data = self.encoder.encoder[i][0].weight.data.clamp(0)
+                self.encoder.mu[0].weight.data = self.encoder.mu[0].weight.data.clamp(0)
+                self.encoder.logvar[0].weight.data = self.encoder.logvar[0].weight.data.clamp(0)
+
+        # compute avg training loss
+        train_loss = running_loss/len(dataloader)
+        return train_loss
+
+    def val_round(self, dataloader, kl_coeff):
+        """
+        Parameters
+        -------------
+        dataloader: pytorch dataloader instance with training data
+        kl_coeff: coefficient for weighting Kullback-Leibler loss
+        """
+        # set to eval mode
+        self.eval()
+
+        # initialize running loss
+        running_loss = 0.0
+
+        with torch.no_grad():
+            # iterate over dataloader for validation
+            for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
+
+                # move batch to device
+                data = data[0].to(self.device)
+
+                # forward step
+                reconstruction, mu, log_var = self.forward(data)
+                loss = self.vae_loss(reconstruction, mu, log_var,data, kl_coeff)
+                running_loss += loss.item()
+
+        # compute avg val loss
+        val_loss = running_loss/len(dataloader)
+        return val_loss
+
+    def train_model(self, trainloader, valloader, lr, kl_coeff, epochs, modelpath, log=True, **kwargs):
+        """
+        Parameters
+        -------------
+        trainloader: pytorch dataloader instance with training data
+        valloader: pytorch dataloader instance with validation data
+        lr: learning rate
+        kl_coeff: coefficient for weighting Kullback-Leibler loss
+        epochs: number of epochs to train the model
+        modelpath: where to store the best model
+        log: if losses should be logged
+        **kwargs: pass the run here if log == True
+        """
+        val_loss_min = float('inf')
+        optimizer = optim.AdamW(self.parameters(), lr = lr)
+
+        for epoch in range(epochs):
+            print(f"Epoch {epoch+1} of {epochs}")
+            train_epoch_loss = self.train_round(trainloader, lr, kl_coeff, optimizer)
+            val_epoch_loss = self.val_round(valloader, kl_coeff)
+            
+            if log == True:
+                run = kwargs.get('run')
+                run["metrics/train/loss"].log(train_epoch_loss)
+                run["metrics/val/loss"].log(val_epoch_loss)
+                
+            if val_epoch_loss < val_loss_min:
+                print('New best model!')
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': self.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': val_epoch_loss,
+                }, modelpath)
+                val_loss_min = val_epoch_loss
+                
+            print(f"Train Loss: {train_epoch_loss:.4f}")
+            print(f"Val Loss: {val_epoch_loss:.4f}")
+
+
+
+
+
---
+++ onto_vae/modules.py
@@ -0,0 +1,311 @@
+#!/usr/bin/env python3
+
+# Modules used by OntoVAE
+
+import numpy as np
+import torch
+from torch import optim
+import torch.nn as nn
+import torch.nn.functional as F
+
+###-------------------------------------------------------------###
+##                       ENCODER CLASS                           ##
+###-------------------------------------------------------------###
+
+class Encoder(nn.Module):
+    """
+    This class constructs an Encoder module for a variational autoencoder.
+
+    Parameters
+    -------------
+    in_features: # of features that are used as input
+    layer_dims: list giving the dimensions of the hidden layers
+    latent_dim: latent dimension
+    drop: dropout rate, default is 0
+    z_drop: dropout rate for latent space, default is 0.5
+    """
+
+    def __init__(self, in_features, latent_dim, layer_dims=[512], drop=0, z_drop=0.5):
+        super(Encoder, self).__init__()
+
+        self.in_features = in_features
+        self.layer_dims = layer_dims
+        self.layer_nums = [layer_dims[i:i+2] for i in range(len(layer_dims)-1)]
+        self.latent_dim = latent_dim
+        self.drop = drop
+        self.z_drop = z_drop
+
+        self.encoder = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.Linear(self.in_features, self.layer_dims[0]),
+                    nn.BatchNorm1d(self.layer_dims[0]),
+                    nn.Dropout(p=self.drop),
+                    nn.ReLU()
+                )
+            ] +
+
+            [self.build_block(x[0], x[1]) for x in self.layer_nums] 
+        )
+
+        self.mu = nn.Sequential(
+            nn.Linear(self.layer_dims[-1], self.latent_dim),
+            nn.Dropout(p=self.z_drop)
+        )
+
+        self.logvar = nn.Sequential(
+            nn.Linear(self.layer_dims[-1], self.latent_dim),
+            nn.Dropout(p=self.z_drop)
+        )
+
+    def build_block(self, ins, outs):
+        return nn.Sequential(
+            nn.Linear(ins, outs),
+            nn.BatchNorm1d(outs),
+            nn.Dropout(p=self.drop),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+
+        # encoding
+        c = x
+        for layer in self.encoder:
+            c = layer(c)
+        #c = c.view(-1, 2, self.latent_dim)
+
+        # get 'mu' and 'log-var'
+        #mu = c[:, 0, :]
+        #log_var = c[:, 1, :]
+
+        mu = self.mu(c)
+        log_var = self.logvar(c)
+
+        return mu, log_var
+
+
+
+###-------------------------------------------------------------###
+##                       DECODER CLASS                           ##
+###-------------------------------------------------------------###
+
+
+
+class Decoder(nn.Module):
+    """
+    This class constructs a Decoder module for a variational autoencoder.
+
+    Parameters
+    -------------
+    in_features: # of features that are used as input
+    layer_dims: list giving the dimensions of the hidden layers
+    latent_dim: latent dimension, default is 128
+    dr: dropout rate, default is 0
+    """
+
+    def __init__(self, in_features, latent_dim, layer_dims=[512], drop=0):
+        super(Decoder, self).__init__()
+
+        self.in_features = in_features
+        self.layer_dims = layer_dims
+        self.layer_nums = [layer_dims[i:i+2] for i in range(len(layer_dims)-1)]
+        self.latent_dim = latent_dim
+        self.drop = drop
+
+        self.decoder = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.Linear(self.latent_dim, self.layer_dims[0]),
+                    nn.BatchNorm1d(self.layer_dims[0]),
+                    nn.Dropout(p=self.drop),
+                    nn.ReLU()
+                )
+            ] +
+
+            [self.build_block(x[0], x[1]) for x in self.layer_nums] +
+
+            [
+                nn.Sequential(
+                    nn.Linear(self.layer_dims[-1], self.in_features),
+                )
+            ]
+        )
+
+    def build_block(self, ins, outs):
+        return nn.Sequential(
+            nn.Linear(ins, outs),
+            nn.BatchNorm1d(outs),
+            nn.Dropout(p=self.drop),
+            nn.ReLU()
+        )
+
+    def forward(self, z):
+
+        # decoding
+        reconstruction = z
+
+        for layer in self.decoder:
+            reconstruction = layer(reconstruction)
+        
+        return reconstruction
+
+
+###-------------------------------------------------------------###
+##                     ONTO ENCODER CLASS                        ##
+###-------------------------------------------------------------###
+
+class OntoEncoder(nn.Module):
+    """
+    This class constructs a Encoder module that is structured like an ontology and following a DAG.
+
+    Parameters
+    --------------
+    in_features: # of features that are used as input
+    layer_dims: list of tuples that define in and out for each layer
+    mask_list: matrix for each layer transition, that determines which weights to zero out
+    drop: dropout rate, default is 0
+    z_drop: dropout rate for latent space, default is 0.5
+    """ 
+
+    def __init__(self, in_features, layer_dims, mask_list, drop=0, z_drop=0.5):
+        super(OntoEncoder, self).__init__()
+
+        self.in_features = in_features
+        self.layer_dims = layer_dims
+        self.layer_shapes = [(np.sum(self.layer_dims[:i+1]), self.layer_dims[i+1]) for i in range(len(self.layer_dims)-1)]
+        self.masks = []
+        for m in mask_list:
+            self.masks.append(m.cuda(0))
+        self.latent_dim = self.layer_dims[-1]
+        self.drop = drop
+        self.z_drop = z_drop
+
+        # Encoder
+        self.encoder = nn.ModuleList(
+
+            [self.build_block(x[0], x[1]) for x in self.layer_shapes[:-1]] 
+
+        ).cuda(0)
+
+        self.mu = nn.Sequential(
+            nn.Linear(self.layer_shapes[-1][0], self.latent_dim),
+            nn.Dropout(p=self.z_drop)
+        ).cuda(0)
+
+        self.logvar = nn.Sequential(
+            nn.Linear(self.layer_shapes[-1][0], self.latent_dim),
+            nn.Dropout(p=self.z_drop)
+        ).cuda(0)
+
+        # apply masks to zero out weights of non-existing connections
+        for i in range(len(self.encoder)):
+            self.encoder[i][0].weight.data = torch.mul(self.encoder[i][0].weight.data, self.masks[i])
+
+        # apply mask on latent space
+        self.mu[0].weight.data = torch.mul(self.mu[0].weight.data, self.masks[-1])
+        self.logvar[0].weight.data = torch.mul(self.logvar[0].weight.data, self.masks[-1])
+
+
+    def build_block(self, ins, outs):
+        return nn.Sequential(
+            nn.Linear(ins, outs),
+            nn.Dropout(p=self.drop)
+        )
+    
+
+    def forward(self, x):
+        
+        # encoding
+        out = x
+
+        for layer in self.encoder:
+            c = layer(out)
+            out = torch.cat((c, out), dim=1)
+
+        #c = c.view(-1, 2, self.latent_dim)
+
+        # get 'mu' and 'log-var'
+        #mu = lat[:, 0, :]
+        #log_var = lat[:, 1, :]
+
+        mu = self.mu(out)
+        log_var = self.logvar(out)
+
+        return mu, log_var
+
+
+
+###-------------------------------------------------------------###
+##                     ONTO DECODER CLASS                        ##
+###-------------------------------------------------------------###
+
+
+class OntoDecoder(nn.Module):
+    """
+    This class constructs a Decoder module that is structured like an ontology and following a DAG.
+  
+    Parameters
+    ---------------
+    in_features: # of features that are used as input
+    study_num: # of different studys that the samples belong to
+    layer_dims: list of tuples that define in and out for each layer
+    mask_list: matrix for each layer transition, that determines which weights to zero out
+    latent_dim: latent dimension
+    drop: dropout rate, default is 0
+    """ 
+
+    def __init__(self, in_features, layer_dims, mask_list, latent_dim, neuronnum=1, drop=0):
+        super(OntoDecoder, self).__init__()
+
+        self.in_features = in_features
+        self.layer_dims = np.hstack([layer_dims[:-1] * neuronnum, layer_dims[-1]])
+        self.layer_shapes = [(np.sum(self.layer_dims[:i+1]), self.layer_dims[i+1]) for i in range(len(self.layer_dims)-1)]
+        self.masks = []
+        for m in mask_list[0:-1]:
+            m = m.repeat_interleave(neuronnum, dim=0)
+            m = m.repeat_interleave(neuronnum, dim=1)
+            self.masks.append(m.cuda(0))
+        self.masks.append(mask_list[-1].repeat_interleave(neuronnum, dim=1).cuda(0))
+        self.latent_dim = latent_dim
+        self.drop = drop
+
+        # Decoder
+        self.decoder = nn.ModuleList(
+
+            [self.build_block(x[0], x[1]) for x in self.layer_shapes[:-1]] +
+
+            [
+                nn.Sequential(
+                    nn.Linear(self.layer_shapes[-1][0], self.in_features)#,
+                    #nn.Sigmoid()
+                )
+            ]
+            ).cuda(0)
+        
+        # apply masks to zero out weights of non-existent connections
+        for i in range(len(self.decoder)):
+            self.decoder[i][0].weight.data = torch.mul(self.decoder[i][0].weight.data, self.masks[i])
+
+        # make all weights in decoder positive
+        for i in range(len(self.decoder)):
+            self.decoder[i][0].weight.data = self.decoder[i][0].weight.data.clamp(0)
+
+    def build_block(self, ins, outs):
+        return nn.Sequential(
+            nn.Linear(ins, outs)#,
+            #nn.Dropout(p=self.drop),
+            #nn.Sigmoid()
+        )
+
+    def forward(self, z):
+
+        # decoding
+        out = z
+
+        for layer in self.decoder[:-1]:
+            c = layer(out)
+            out = torch.cat((c, out), dim=1)
+        reconstruction = self.decoder[-1](out)
+        
+        return reconstruction
+
